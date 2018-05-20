69d0ad6d29f6d6b6a9460121e2c376622c21868b
Randall Robinson <keith_adams007@yahoo.com>
2018-05-20 17:24:10 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -3,3 +3,7 @@ function driversWithRevenueOver (array, revenue) {
   const newarr =  array.filter (function (element) { return  element['revenue'] > revenue})
   return newarr
 }
+
+function driverNamsWithRevenueOver (array, revenue) {
+  const newarr = array.map (function (element) { return element['name'] if })
+}
