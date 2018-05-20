993110ab2f668f0f4af5ead20c31875335e4e455
Randall Robinson <keith_adams007@yahoo.com>
2018-05-20 17:22:25 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,5 @@
 // Code your solution here:
+function driversWithRevenueOver (array, revenue) {
+  const newarr =  array.filter (function (element) { return  element['revenue'] > revenue})
+  return newarr
+}
